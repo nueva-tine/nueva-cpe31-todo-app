5c2bff509fa5dc11ef73361995dbdf1b6136eb6d
Christine Grace <ncc0403@dlsud.edu.ph>
2024-01-09 10:08:22 +0800

    Initial Commit

--- lib/constants/app_style.dart
+++ lib/constants/app_style.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppStyle{
-  static Color bgColor = Color(0xFFC9ACE0);
-  static Color mainColor = Color(0xFFD4A1F6);
-  static Color accentColor = Color(0xFFBE65BD);
 
   static TextStyle mainTitle = GoogleFonts.roboto(fontSize:40.0, fontWeight:FontWeight.bold);
 
